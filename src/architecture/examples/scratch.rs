e6a9255afd3a6b357d70a22b3fd3678e34da4c28
rayyankhan1428 <rayyankhan1428h@gmail.com>
2024-03-22 10:50:32 -0400

    Added basic parser code

---
+++ src/architecture/examples/scratch.rs
@@ -0,0 +1,9 @@
+extern crate architecture;
+
+use architecture::operation;
+
+fn main() {
+    let reg_set_from_imm_parser = operation::Parser {
+        
+    };
+}
--- src/architecture/src/operation.rs
+++ src/architecture/src/operation.rs
@@ -1,7 +1,22 @@
-pub struct OperationParser {
-    opcode: u8, 
-    r0_expected: bool,
-    r1_expected: bool,
-    imm_expected: bool,
-    imm_size: bool
+pub trait ByteStream {
+    // Get the next byte after the previous byte or initial byte.
+    fn get_next(&mut self) -> u8;
+
+    // Get a byte relative to the current byte cursor by index.
+    fn get_relative(&mut self, position: isize) -> u8;
+
+    fn set_cursor(&mut self, cursor: usize);
+}
+
+pub struct Parser<'a> {
+    pub byte_stream: &'a dyn ByteStream,
+    pub opcode: u8,
+    pub r0_expected: bool,
+    pub r1_expected: bool,
+    pub imm_expected: bool,
+    pub imm_size: u8 // How many bytes the imm should be
+}
+
+impl<'a> Parser<'a> {
+    
 }
